e9bd1713e05a9c31fcb5322c69050c57e34c54bb
ROCHEBILLARD-C <crochebillard@gmail.com>
2025-06-07 19:06:35 +0200

    Ajout d'un script Teleportation
    Création d'un script de téléportation d'un objet quand un trigger est activé

---
+++ Projet/Assets/Scripts/TeleportObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Le but de cette classe est de gérer la téléportation d'objet quand un trigger est activé
+//Il faut appliquer ce script à l'objet qui contient le trigger
+public class TeleportObject : MonoBehaviour
+{
+    // On stock la position désiré après téléportation en ce basant sur les coordoné global
+    public Vector3 destination;
+
+    // On choisi la cible à téléporter
+    public GameObject objetTeleporte;
+
+    private Transform tran; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tran = objetTeleporte.GetComponent<Transform> ();        
+
+        tran.position = destination;
+
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        tran.position = destination;
+    }
+
+
+}
